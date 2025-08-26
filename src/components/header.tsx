7727c062b26e89ead5bf2c4f7d9d57510f1b744d
Teachers'Co operative Society <sarikhor94@gmail.com>
2025-08-26 17:40:53 +0000

    Warning: Missing `Description` or `aria-describedby={undefined}` for {Di

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,7 +26,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Logo } from "./logo";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -112,6 +112,7 @@ export function Header({ user }: { user: User | null }) {
                 </SheetTrigger>
                 <SheetContent side="left">
                     <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
+                    <SheetDescription className="sr-only">A list of navigation links for the site.</SheetDescription>
                     <nav className="grid gap-6 text-lg font-medium">
                         <Link
                             href={user?.role === 'admin' ? '/admin/dashboard' : '/dashboard'}
