b2204298b0dfa7057fe91b1f0cdc86f5de30ac5d
Teachers'Co operative Society <sarikhor94@gmail.com>
2025-08-26 18:02:23 +0000

    remove the navigation bar for every user

--- src/components/app-provider.tsx
+++ src/components/app-provider.tsx
@@ -1,10 +1,8 @@
-
 "use client";
 
 import * as React from "react";
 import { usePathname } from "next/navigation";
 import type { User } from "@/lib/types";
-import { Header } from "./header";
 
 export function AppProvider({ children, user }: { children: React.ReactNode, user: User | null }) {
   const pathname = usePathname();
@@ -16,7 +14,6 @@ export function AppProvider({ children, user }: { children: React.ReactNode, use
 
   return (
     <div className="flex min-h-screen w-full flex-col">
-        <Header user={user} />
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
             {children}
         </main>
